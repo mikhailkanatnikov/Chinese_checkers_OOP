d961f95dfdb674bcf81d86fb6e8baac4e3f05922
Mikhail_Kanatnikov <kanatnikovmihan@mail.ru>
2025-12-12 17:01:10 +0300

    первый комит с проектом

---
+++ src/Checker.java
@@ -0,0 +1,18 @@
+public class Checker {
+
+    Position position;
+    PlayerColor color;
+
+    public Checker(Position position, PlayerColor color){
+        this.position = position;
+        this.color = color;
+    }
+
+    
+
+
+
+
+}
+
+
